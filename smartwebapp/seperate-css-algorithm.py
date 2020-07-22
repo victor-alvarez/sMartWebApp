c81b71106b7c689dad736e34c48a45a2206576b0
Victor <victor.alvarez@mail.utoronto.ca>
2020-07-22 00:22:40 -0400

    Moved all CSS code within home.html into main.css

---
+++ smartwebapp/seperate-css-algorithm.py
@@ -0,0 +1,22 @@
+app = input("Enter the app name: ")
+filename = input("Enter the HTML file name: ")
+
+f1 = open(app + "/templates/" + app + "/" + filename, "r", encoding="utf8")
+f2 = open(app + "/static/" + app + "/" + "main.css", "a", encoding="utf8")
+
+s = f1.read()
+
+while "<style" in s:
+    i1 = s.find("<style")
+    i2 = s.find(">", i1)
+    j1 = s.find("</style")
+    j2 = s.find(">", j1)
+    f2.write(s[i2 + 1:j1])
+    s = s[:i1] + s[j2 + 1:]
+
+f1.close()
+f2.close()
+
+f1 = open(app + "/templates/" + app + "/" + filename, "w", encoding="utf8")
+f1.write(s)
+f1.close()
