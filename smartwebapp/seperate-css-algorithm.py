9463f49c824af615a58aeccf9d4e94a29733b6b8
Victor <victor.alvarez@mail.utoronto.ca>
2020-07-28 00:39:27 -0400

    Updated site with the latest styles and html

--- smartwebapp/seperate-css-algorithm.py
+++ smartwebapp/seperate-css-algorithm.py
@@ -8,7 +8,7 @@ s = f1.read()
 while "<style" in s:
     i1 = s.find("<style")
     i2 = s.find(">", i1)
-    j1 = s.find("</style")
+    j1 = s.find("</style", i2)
     j2 = s.find(">", j1)
     f2.write(s[i2 + 1:j1])
     s = s[:i1] + s[j2 + 1:]
