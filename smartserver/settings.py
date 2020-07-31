5836db4ce024b8e3db9b69a755fee2304b8d02ce
Victor <victor.alvarez@mail.utoronto.ca>
2020-07-30 23:00:17 -0400

    Removed prerelease app and added backend, web, and mobile apps. Also, added a .gitignore file.

--- smartserver/settings.py
+++ smartserver/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'prerelease.apps.PrereleaseConfig',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
